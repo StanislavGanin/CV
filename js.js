d8d7c6163dce1f41f16e38332949ef648faa99ce
stanisalvganin <rndqa@yandex.ru>
2022-12-20 10:59:36 +0300

    ver4

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Viam supervadet vadens!')
+	alert('Viam supervadet vadens!!!')
 })
